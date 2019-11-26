8d2bbe72130d7eb56523448576b6ba8af36d9ef2
Mantas <syenergyy@gmail.com>
2019-11-26 12:02:00 +0200

    Add version, change copyright strings

--- AI_PovX/Properties/AssemblyInfo.cs
+++ AI_PovX/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mantas-2155X aka 2155X")]
 [assembly: AssemblyProduct("AI_PovX")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Mantas-2155X aka 2155X Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion(AI_PovX.AI_PovX.VERSION)]
 [assembly: AssemblyFileVersion("1.0.0.0")]
